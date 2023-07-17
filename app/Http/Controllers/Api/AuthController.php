a35cd7aadb56cc50084999cc61ad272e6bd77f40
dev_sultan <sutlan.asim04@gmail.com>
2023-07-17 12:49:49 -0700

    first commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json(['message' => 'User logged out successfully']);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if (!Auth::attempt($credentials)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $user = $request->user();
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return response()->json(['access_token' => $token]);
+    }
+
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'User registered successfully.',
+            'access_token' => $token,
+        ]);
+    }
+}
---
+++ resources/views/pusher_information.blade.php
@@ -0,0 +1,11 @@
+<?php
+
+//app_name = able-briars-199
+
+/**
+ * 
+ * able-briars-199-development
+    * able-briars-199-staging
+ * able-briars-199-production
+ * 
+ * 
